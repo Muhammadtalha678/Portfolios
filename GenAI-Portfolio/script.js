abb0135310abcba671ffbeaf318fd5673e6b7649
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-04-28 12:58:17 +0500

    GenAI Portfolio

--- GenAI-Portfolio/script.js
+++ GenAI-Portfolio/script.js
@@ -9,8 +9,11 @@
         contentContainer.style.display = 'block';
     loader.style.display = 'none';
     body.style.backgroundImage = "url('image.jpg')";
-    }, 2000);
-        
+}, 2000);
+
+//create elements inside content-id
+var content = document.getElementById('content-id');
+//TypeScript Assignments
 var assignments = [
     {
         title: "Typescript Assignment 45 Questions",
@@ -39,13 +42,13 @@ var assignments = [
         title: "CLI Atm Machine",
         desc: "CLI Atm Machine using NodeJs\Typescript use Inquirer to take user input.",
         link: "https://github.com/Muhammadtalha678/ATM"
-        }
+    }
     ,
     {
         title: "CLI Todo List",
         desc: "CLI Todo List using NodeJs\Typescript use Inquirer to take user input.",
         link: "https://github.com/Muhammadtalha678/Todo-List"
-        }
+    }
     ,
     {
         title: "CLI Currency Converter",
@@ -58,37 +61,95 @@ var assignments = [
         desc: "CLI Word Counter using NodeJs\Typescript use Inquirer to take user input.",
         link: "https://github.com/Muhammadtalha678/word-counter"
         }
-    ,
-    {
+        ,
+        {
         title: "CLI BMI Calculator",
         desc: "CLI Word Counter using NodeJs\Typescript use Inquirer to take user input.",
         link: "https://github.com/Muhammadtalha678/Bmi_Calculator "
         }
 ]
-var projectCards = document.getElementById('project-cards-id');
-for (var i = 0; i < assignments.length; i++) {
-    var divCard = document.createElement("div");
-    divCard.className = 'project-card'
+
+
+if (assignments.length > 0) {
+    //create div for section heading
+    var createSectionHeading = document.createElement('div')
+    createSectionHeading.className = 'section-heading'
+    createSectionHeading.textContent = 'Typescript Projects:'
     
-    var divCircle = document.createElement("div");
-    divCircle.className = 'circle'
-    divCircle.textContent = i + 1
-    
-    var h2 = document.createElement("h2");
-    h2.textContent = assignments[i].title
-    
-    var para = document.createElement("p");
-    para.textContent = assignments[i].desc
+    //create div for project card
+    var createProjectCards = document.createElement('div')
+    createProjectCards.className = 'project-cards'
+    createProjectCards.id = 'project-cards-id'
+    content.appendChild(createSectionHeading)
+    content.appendChild(createProjectCards)
+    for (var i = 0; i < assignments.length; i++) {
+        var divCard = document.createElement("div");
+        divCard.className = 'project-card'
+
+        var divCircle = document.createElement("div");
+        divCircle.className = 'circle'
+        divCircle.textContent = i + 1
+
+        var h2 = document.createElement("h2");
+        h2.textContent = assignments[i].title
+
+        var para = document.createElement("p");
+        para.textContent = assignments[i].desc
 
         var anchor = document.createElement("a");
-    anchor.textContent = 'View on GitHub';
-    anchor.href = assignments[i].link;
-    anchor.target = '_blank'; // Open link in new tab
+        anchor.textContent = 'View on GitHub';
+        anchor.href = assignments[i].link;
+        anchor.target = '_blank'; // Open link in new tab
+
+        divCard.appendChild(divCircle)
+        divCard.appendChild(h2)
+        divCard.appendChild(para)
+        divCard.appendChild(anchor)
+
+        createProjectCards.appendChild(divCard)
+    }
+
+}
+var nextJsAssignments = [
     
-    divCard.appendChild(divCircle)    
-    divCard.appendChild(h2)    
-    divCard.appendChild(para)    
-    divCard.appendChild(anchor)
+]
+
+
+if (nextJsAssignments.length> 0) {
+    var createSectionHeading = document.createElement('div')
+    createSectionHeading.className = 'section-heading'
+    createSectionHeading.textContent = 'NextJs Projects:'
+    content.appendChild(createSectionHeading)
     
-    projectCards.appendChild(divCard)
+    var createProjectCards = document.createElement('div')
+    createProjectCards.className = 'project-cards'
+    createProjectCards.id = 'project-cards-id'
+    content.appendChild(createProjectCards)
+    for (let index = 0; index < nextJsAssignments.length; index++) {
+        var divCard = document.createElement("div");
+        divCard.className = 'project-card'
+
+        var divCircle = document.createElement("div");
+        divCircle.className = 'circle'
+        divCircle.textContent = index + 1
+
+        var h2 = document.createElement("h2");
+        h2.textContent = nextJsAssignments[index].title
+
+        var para = document.createElement("p");
+        para.textContent = nextJsAssignments[index].desc
+
+        var anchor = document.createElement("a");
+        anchor.textContent = 'View on GitHub';
+        anchor.href = nextJsAssignments[index].link;
+        anchor.target = '_blank'; // Open link in new tab
+
+        divCard.appendChild(divCircle)
+        divCard.appendChild(h2)
+        divCard.appendChild(para)
+        divCard.appendChild(anchor)
+
+        createProjectCards.appendChild(divCard)
+        
+    }
 }
